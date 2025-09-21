a30d1d70957eab4b8771ea296b5f52082c02c4a4
adityapachaury <149651486+adityapachaury@users.noreply.github.com>
2025-09-21 08:37:03 +0530

    Delete Java_String directory

--- Java_String/Level_2/ToLowerCase.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class ToLowerCase {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String str = sc.nextLine();
-        System.out.println("Lowercase: " + str.toLowerCase());
-    }
-}
